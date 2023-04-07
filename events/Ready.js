a4eef298c099fbdee450f5b22c783013d5cf1385
gisketch <agjmnz@gmail.com>
2023-04-08 04:45:20 +0800

    eventlisteners

---
+++ events/Ready.js
@@ -0,0 +1,13 @@
+const { Events, ActivityType } = require('discord.js');
+
+module.exports = {
+	name: Events.ClientReady,
+	once: true,
+	execute(client) {
+		console.log(`Ready! Logged in as ${client.user.tag}`);
+        client.user.setPresence({
+            activities: [{ name: `a Steaming Siomai`, type: ActivityType.Watching }],
+            status: 'UwU 🍥',
+          });
+	},
+};
