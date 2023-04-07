e410390bd2f47639fdab526ad8799f1f234e3bbd
gisketch <agjmnz@gmail.com>
2023-04-08 02:50:43 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,47 @@
 const dotenv = require('dotenv');
-
 dotenv.config();
-console.log(process.env.DISCORD_TOKEN);
+
+//Initialize environmental variables
+
+TOKEN = process.env.DISCORD_TOKEN
+CLIENT_ID = "1093966109838946374"
+
+const { REST, Routes } = require('discord.js');
+
+const commands = [
+  {
+    name: 'ping',
+    description: 'Replies with Pong!',
+  },
+];
+
+const rest = new REST({ version: '10' }).setToken(TOKEN);
+
+(async () => {
+  try {
+    console.log('Started refreshing application (/) commands.');
+
+    await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
+
+    console.log('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
+
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
+});
+
+client.login(TOKEN);
