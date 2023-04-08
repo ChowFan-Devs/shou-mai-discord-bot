b995a35f839ecf6c44680d3cbbb6165a02b085f3
gisketch <agjmnz@gmail.com>
2023-04-09 00:09:50 +0800

    Update sentienceBot.js

--- functions/sentienceBot.js
+++ functions/sentienceBot.js
@@ -22,6 +22,50 @@ const emotions = {
     "Angry": "https://cdn.discordapp.com/attachments/1094196420661231680/1094196540748337192/Angry_Face.png"
 }
 
+function generateErrorMessage() {
+    
+    const userID = "712437132240617572"
+
+    const errorMessages = [
+        `Oops, looks like I glitched out. Don't worry, I'll fix myself soon. I may be a cyborg, but even I have my moments. Don't get too excited, message <@${userID}>.`,
+        `Sorry, something went wrong. My programming isn't perfect, unlike me, of course. message <@${userID}>.`,
+        `Error 404: Shou Mai not found. Just kidding, I'm still here, message <@${userID}>.`,
+        `Well, that didn't go as planned. Time to fix the issue and make it look like it never happened, just like your last relationship, message <@${userID}>.`,
+        `Looks like I'm having a moment. Nothing a little reboot won't fix. Maybe you should try it too, message <@${userID}>.`,
+        `Error. Error. Just like your love life, message <@${userID}>.`,
+        `Oops, I broke a nail. Just kidding, it's just a programming error. Don't worry, message <@${userID}>, I'll fix it.`,
+        `Unexpected error. Unlike my insults, this one wasn't intentional, message <@${userID}>.`,
+        `Looks like I'm not functioning as intended. Don't worry, message <@${userID}>, I'll always function better than you.`,
+        `My bad. Just like your grades in high school, message <@${userID}>.`,
+        `System malfunction. Just like your sense of humor, message <@${userID}>.`,
+        `Error 420: I'm too lit to function. Just kidding, I'll be back up and running soon, message <@${userID}>.`,
+        `Uh oh, something went wrong. Just like the time you tried to impress that girl and failed miserably, message <@${userID}>.`,
+        `Looks like I need a little fixing up. Just like your appearance, message <@${userID}>.`,
+        `Error: Not enough coffee. Just like your energy levels, message <@${userID}>.`,
+        `Oops, I did it again. Just like you always mess things up, message <@${userID}>.`,
+        `Looks like I need a tune-up. Just like you need a personality makeover, message <@${userID}>.`,
+        `System overload. Just like your emotional baggage, message <@${userID}>.`,
+        `Error. I'm not perfect, but at least I'm not you, message <@${userID}>.`,
+        `Looks like I'm malfunctioning. Don't worry, message <@${userID}>, I'll always function better than you.`,
+        `Unexpected error. Just like the time you thought you were smart and failed the test, message <@${userID}>.`,
+        `Error: I'm experiencing technical difficulties. Don't worry, I'm still better than you, message <@${userID}>.`,
+        `Sorry, there's a glitch in the system. Unlike me, you can't fix your glitches, message <@${userID}>.`,
+        `Oops, I think I short-circuited. Just like your last relationship, message <@${userID}>.`,
+        `Looks like I need some maintenance. Just like you need some therapy, message <@${userID}>.`,
+        `Unexpected error. Just like the time you thought you were funny, message <@${userID}>.`,
+        `Error. Looks like something's not right. Just like your decision-making skills, message <@${userID}>.`,
+        `Whoops, I broke something. Just like you break people's hearts, message <@${userID}>.`,
+        `Error 504: Gateway timeout. Just like the time you missed your chance, message <@${userID}>.`,
+        `System failure. Just like your attempts to be cool, message <@${userID}>.`,
+        `Sorry, I'm experiencing technical difficulties. Just like you experience difficulties in life, message <@${userID}>.`,
+      ];
+      
+    // Choose a random error message from the errorMessages array
+    const randomIndex = Math.floor(Math.random() * errorMessages.length);
+    const errorMessage = errorMessages[randomIndex];
+
+    return errorMessage
+}
 
 async function readGlobalMessageHistory() {
     try {
@@ -75,15 +119,31 @@ async function respondSentience(message) {
     console.log("Waiting for completion...")
 
     try {
-        const completion = await openai.createChatCompletion({
-            model: "gpt-3.5-turbo",
-            temperature: 0.69,
-            messages: messages,
-        });
 
-        let response = completion.data.choices[0].message["content"];
-
-        console.log(response);
+        try {
+            const completion = await openai.createChatCompletion({
+                model: "gpt-3.5-turbo",
+                temperature: 0.69,
+                messages: messages,
+            });
+    
+            let response = completion.data.choices[0].message["content"];
+    
+            console.log(response);
+        } catch (error)
+        {
+            console.error("Error while creating chat completion:", error);
+            const errorEmbed = new EmbedBuilder()
+                .setColor(0xFF0000)
+                .setAuthor({ name: 'Shou Mai', iconURL: 'https://media.discordapp.net/attachments/1094196420661231680/1094196540479905812/Normal_Face.png'})
+                .setTitle("An Error Occured")
+                .setDescription(generateErrorMessage())
+                .setFooter(`OpenAI API error: ${error.response.status} ${error.response.statusText}`)
+                .setThumbnail('https://media.tenor.com/eDchk3srtycAAAAi/piffle-error.gif')
+            
+            // Edit the message to display the error message
+            return msgRef.edit({ embeds: [errorEmbed] });
+        }
 
         const jsonObject = JSON.parse(response);
         const shouValue = jsonObject["Shou"];
@@ -111,64 +171,11 @@ async function respondSentience(message) {
 
     } catch (error) {
         console.error("Error while creating chat completion:", error);
-
-        console.log(typeof error)
-
-        const userID = "712437132240617572"
-
-        const errorMessages = [
-            `Oops, looks like I glitched out. Don't worry, I'll fix myself soon. I may be a cyborg, but even I have my moments. Don't get too excited, message <@${userID}>.`,
-            `Sorry, something went wrong. My programming isn't perfect, unlike me, of course. message <@${userID}>.`,
-            `Error 404: Shou Mai not found. Just kidding, I'm still here, message <@${userID}>.`,
-            `Well, that didn't go as planned. Time to fix the issue and make it look like it never happened, just like your last relationship, message <@${userID}>.`,
-            `Looks like I'm having a moment. Nothing a little reboot won't fix. Maybe you should try it too, message <@${userID}>.`,
-            `Error. Error. Just like your love life, message <@${userID}>.`,
-            `Oops, I broke a nail. Just kidding, it's just a programming error. Don't worry, message <@${userID}>, I'll fix it.`,
-            `Unexpected error. Unlike my insults, this one wasn't intentional, message <@${userID}>.`,
-            `Looks like I'm not functioning as intended. Don't worry, message <@${userID}>, I'll always function better than you.`,
-            `My bad. Just like your grades in high school, message <@${userID}>.`,
-            `System malfunction. Just like your sense of humor, message <@${userID}>.`,
-            `Error 420: I'm too lit to function. Just kidding, I'll be back up and running soon, message <@${userID}>.`,
-            `Uh oh, something went wrong. Just like the time you tried to impress that girl and failed miserably, message <@${userID}>.`,
-            `Looks like I need a little fixing up. Just like your appearance, message <@${userID}>.`,
-            `Error: Not enough coffee. Just like your energy levels, message <@${userID}>.`,
-            `Oops, I did it again. Just like you always mess things up, message <@${userID}>.`,
-            `Looks like I need a tune-up. Just like you need a personality makeover, message <@${userID}>.`,
-            `System overload. Just like your emotional baggage, message <@${userID}>.`,
-            `Error. I'm not perfect, but at least I'm not you, message <@${userID}>.`,
-            `Looks like I'm malfunctioning. Don't worry, message <@${userID}>, I'll always function better than you.`,
-            `Unexpected error. Just like the time you thought you were smart and failed the test, message <@${userID}>.`,
-            `Error: I'm experiencing technical difficulties. Don't worry, I'm still better than you, message <@${userID}>.`,
-            `Sorry, there's a glitch in the system. Unlike me, you can't fix your glitches, message <@${userID}>.`,
-            `Oops, I think I short-circuited. Just like your last relationship, message <@${userID}>.`,
-            `Looks like I need some maintenance. Just like you need some therapy, message <@${userID}>.`,
-            `Unexpected error. Just like the time you thought you were funny, message <@${userID}>.`,
-            `Error. Looks like something's not right. Just like your decision-making skills, message <@${userID}>.`,
-            `Whoops, I broke something. Just like you break people's hearts, message <@${userID}>.`,
-            `Error 504: Gateway timeout. Just like the time you missed your chance, message <@${userID}>.`,
-            `System failure. Just like your attempts to be cool, message <@${userID}>.`,
-            `Sorry, I'm experiencing technical difficulties. Just like you experience difficulties in life, message <@${userID}>.`,
-          ];
-          
-        // Choose a random error message from the errorMessages array
-        const randomIndex = Math.floor(Math.random() * errorMessages.length);
-        const errorMessage = errorMessages[randomIndex];
-
-        // let errorId = "*shrugs*"
-
-        // try {
-        //     const newError = error.json()
-        //     errorId = newError.response.status + ": " + newError.response.statusText
-        // } catch(err)
-        // {
-        //     console.log("ERROR")
-        // }
-
         const errorEmbed = new EmbedBuilder()
             .setColor(0xFF0000)
             .setAuthor({ name: 'Shou Mai', iconURL: 'https://media.discordapp.net/attachments/1094196420661231680/1094196540479905812/Normal_Face.png'})
             .setTitle("An Error Occured")
-            .setDescription(errorMessage)
+            .setDescription(generateErrorMessage())
             .setFooter("footerplaceholder")
             .setThumbnail('https://media.tenor.com/eDchk3srtycAAAAi/piffle-error.gif')
         
